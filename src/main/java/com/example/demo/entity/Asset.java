a96e35c72e234fa439f5c917a2a3375c8fce0773
bala-amypo <bala@amypo.in>
2025-12-19 03:39:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/Asset.java
+++ src/main/java/com/example/demo/entity/Asset.java
@@ -5,9 +5,11 @@ public class Asset{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+    @Column(unique=true,nullable=false)
     private String assertTag;
     private String assertType;
     private String model;
     private LocalDate purchaseDate;
     private String status;
+    @ManyToOne
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -2,4 +2,15 @@ package com.example.demo.entity;
 
 public class User{
     
+@Entity
+public class Asset{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique=true,nullable=false)
+    private String assertTag;
+    private String assertType;
+    private String model;
+    private LocalDate purchaseDate;
+    private String status;
 }
