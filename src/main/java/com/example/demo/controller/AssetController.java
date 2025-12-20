af4e38a163766a00b757193ba607eca99e3eeba9
bala-amypo <bala@amypo.in>
2025-12-20 13:15:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/AssetController.java
+++ src/main/java/com/example/demo/controller/AssetController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 @RestController
-
+@RequestMapping("/api/assests
 public class AssetController{
 @Autowired StudentService ser;
 @PostMapping("/post")
