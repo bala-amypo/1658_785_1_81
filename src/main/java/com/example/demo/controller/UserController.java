d5155d744630a364ad46c80719ae2c30e3fb05fd
bala-amypo <bala@amypo.in>
2025-12-20 13:49:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,38 @@
 package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.UserController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.demo.service.UserService;
+import com.example.demo.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/assests")
 public class UserController{
-    
+    private final AssetService assetService;
+    public AssetController(AssetService assetService){
+    this.assetService=assetService;
+@PostMapping("/post")
+public Asset sendData(@RequestBody Asset asset){
+    return assetService.CreateAsset(asset);
+}
+@GetMapping("/get")
+public List<Asset>getval(){
+    return assetService.getAllAsset();
+}
+
+@GetMapping("/getid/{id}")
+public Asset getdata(@PathVariable Long id){
+    return assetService.getAsset(id);
+}
+@PutMapping("/putid/{id}")
+public Asset putval(@PathVariable Long id,@RequestBody String status){
+    return assetService.updateStatus(id,status);
+}
 }
