2ec1f3a550178041e256672aec76b5be50df8c4d
bala-amypo <bala@amypo.in>
2025-12-20 08:17:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/LifecycleEventRepository.java
+++ src/main/java/com/example/demo/repository/LifecycleEventRepository.java
@@ -2,3 +2,14 @@ package com.example.demo.repository;
 public interface LifecycleEventRepository{
     
 }
+package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.Asset;
+import java.util.List;
+@Repository
+public interface LifecycleEventRepository extends JpaRepository<Asset,Long>{
+List<Asset> findByStatus(String status);
+    
+}
