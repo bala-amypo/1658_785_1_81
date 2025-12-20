8763b6f750850423a2f59439554357def055c0af
bala-amypo <bala@amypo.in>
2025-12-20 14:26:54 +0000

    User commit

--- src/main/java/com/example/demo/service/DisposalRecordService.java
+++ src/main/java/com/example/demo/service/DisposalRecordService.java
@@ -1,5 +1,9 @@
 package com.example.demo.service;
 
-public class DisposalRecordService{
-    
+import com.example.demo.entity.DisposalRecord;
+import java.util.List;
+public interface DisposalRecordService{
+DisposalRecord PostData(Long assetId,DisposalRecord disposal);
+List<DisposalRecord>getval();
+DisposalRecord getdata(Long id);
 }
--- src/main/java/com/example/demo/service/TransferRecordService.java
+++ src/main/java/com/example/demo/service/TransferRecordService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import com.example.demo.entity.TransferRecord;
 import java.util.List;
 public interface TransferRecordService{
-TransferRecord PostData(Long assetId,Long userId,LiecycleEvent event);
+TransferRecord PostData(Long assetId,TransferRecord record);
 List<TransferRecord>getval(Long assetId);
 TransferRecord getdata(Long id);
 }
