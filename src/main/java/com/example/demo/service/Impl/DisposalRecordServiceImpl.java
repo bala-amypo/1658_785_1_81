1b334feade79785ed0cb20ee7b1a748323d708b8
bala-amypo <bala@amypo.in>
2025-12-20 16:39:11 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/DisposalRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/DisposalRecordServiceImpl.java
@@ -1,4 +1,43 @@
 package com.example.demo.service.Impl;
-public class DisposalRecordServiceImpl{
+import org.springframework.stereotype.Service;
+import com.example.demo.service.TransferRecordService;
+import com.example.demo.entity.DisposalRecord;
+import com.example.demo.exception.ResourceNotException;
+
+import com.example.demo.repository.TransferRecordRepository;
+import java.util.List;
+@Service
+public class DisposalRecordServiceImp implements TransferRecordService{
+private final TransferRecordRepository transferRepo;
+private final final AssetRepository assetRepo;
+private final final UserRepository userRepo;
+
+public TransferRecordServiceImpl(TransferRecordRepository,transferRepo,AssetRepository assetRepo,UserRepository userRepo){
+    this.userRepo=userRepo;
+    this.assetRepo=assetRepo;
+    this.transferRepo=transferRepo;
+}
+    @Override
+public TransferRecord PostData(int assetId,TransferRecord record){
+    Asset asset=assetRepo.findById((long)id).orElseThrow(()->new ResorceNotFoundException("Asset not found")
+user admin=userRepo.findById(record.getApprovedBy().getId()).orElseThrow(()->new ResorceNotFoundException("User not found"));
+if(!"ADMIN".equals(admin.getRole())){
+    throw new ValidatinException("Approver must be ADMIN");
+}
+if(record.getFromDepartment().equals(record.getToDepartment())){
+    throw new ValidatinException("Departments must differ");
+    }
+    if(record.getTransferDate().isAfter(LocalDate.now())){
+    throw new ValidationException("Transfer date cannot be in the future");
+    }
+record.setAsset(asset);
+record.setApprovedBy(admin);
+return transferRepo.save(record);
+}
+
+@Override
+public List<TransferRecord>getValByAsset(int assetId){
+return transferRepo.findByAssetId((long)assetId);
+}
 
 }
