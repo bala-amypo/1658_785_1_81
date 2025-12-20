a6d7c657934bfdb86ed8e1222f84c0e2c3e4b5ff
bala-amypo <bala@amypo.in>
2025-12-20 13:40:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/LifecycleEventController.java
+++ src/main/java/com/example/demo/controller/LifecycleEventController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.LifecycleEventController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
