1438529f7a9637575e32fb425b03f51b993712cc
bala-amypo <bala@amypo.in>
2025-12-20 16:12:11 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/AssetServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/AssetServiceImpl.java
@@ -1,5 +1,4 @@
 package com.example.demo.service.Impl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.service.AssetService;
 import com.example.demo.entity.Asset;
--- src/main/java/com/example/demo/service/Impl/LifecycleEventServiceImpl..java
+++ src/main/java/com/example/demo/service/Impl/LifecycleEventServiceImpl..java
@@ -1,21 +1,17 @@
 package com.example.demo.service.Impl;
-
-public class LifecycleEventServiceImpl{
-
-}
-package com.example.demo.service.Impl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.service.UserService;
-import com.example.demo.entity.User;
+import com.example.demo.service.LifecycleEventService;
+import com.example.demo.entity.LifecycleEvent;
 import com.example.demo.exception.ResourceNotException;
 
 import com.example.demo.repository.UserRepository;
 import java.util.List;
 @Service
-public class UserServiceImpl implements UserService{
-private final UserRepository userRepo;
-private final PasswordEncoder passwordEncoder;
+public class LifecycleEventServiceImp implements LifecycleEventService{
+private final LifecycleEventRepository eventRepo;
+private final final AssetRepository assetRepo;
+private final final AssetRepository assetRepo;
+
 public UserServiceImpl(UserRepository,userRepo,PasswordEncoder passwordEncoder){
     this.userRepo=userRepo;
     this.passwordEncoder=passwordEncoder;
