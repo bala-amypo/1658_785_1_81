f05e3f32a50ebd3b372688b1fa237a65a2dfd04c
bala-amypo <bala@amypo.in>
2025-12-20 15:51:10 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -1,5 +1,35 @@
+
 package com.example.demo.service.Impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.service.UserServiceService;
+import com.example.demo.entity.Asset;
+import com.example.demo.repository.AssetRepository;
+import java.util.List;
+@Service
+public class UserServiceImpl implements AssetService{
+private final AssertRepository assetRepo;
+public AssetServiceImpl(AssetRepository assetRepo){
+    this.assetRepo=assetRepo;
+}
+    @Override
+public Asset PostData(Asset asset){
+return assetRepo.save(asset);
+}
+@Override
+public List<Asset>getAllData(){
+return assetRepo.findAll();
+}
 
-public class UserServiceImpl{
+@Override
+public Asset getData(int id){
+    return assetRepo.findById((long)id).orElseThrow(()->new ResorceNotFoundException("Asset not found"));
 
+}
+@Override
+public Asset updateStatus(int id,String status){
+   Asset asset=getData(id);
+   asset.setStatus(status);
+   return assetRepo.save(asset);
+}
 }
