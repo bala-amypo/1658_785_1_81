5421ccaa998139420db7c34d220476dbc69a2350
bala-amypo <bala@amypo.in>
2025-12-20 13:34:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/LifecycleEventController.java
+++ src/main/java/com/example/demo/controller/LifecycleEventController.java
@@ -1,4 +1,39 @@
 package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.LifecycleEventController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.demo.service.LifecycleEventService;
+import com.example.demo.entity.LifecycleEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/events")
 public class LifecycleEventController{
-    
+    private final LifecycleEventService eventService;
+    public LifecycleEventController(LifecycleEventService eventService){
+    this.eventService=eventService;
+@PostMapping("/post/")
+public Asset sendData(@RequestBody Asset asset){
+    return assetService.CreateAsset(asset);
+}
+@GetMapping("/get")
+public List<Asset>getval(){
+    return assetService.getAllAsset();
+}
+
+@GetMapping("/getid/{id}")
+public Asset getdata(@PathVariable Long id){
+    return assetService.getAsset(id);
+}
+@PutMapping("/putid/{id}")
+public Asset putval(@PathVariable Long id,@RequestBody String status){
+    return assetService.updateStatus(id,status);
+}
 }
