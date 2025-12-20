874b65ae874031fc7540c4ed0879793b5c0640bd
bala-amypo <bala@amypo.in>
2025-12-20 14:18:46 +0000

    User commit

--- src/main/java/com/example/demo/service/AssetService.java
+++ src/main/java/com/example/demo/service/AssetService.java
@@ -1,4 +1,10 @@
 package com.example.demo.service;
-public class AssetService{
-    
+
+import com.example.demo.entity.Asset;
+import java.util.List;
+public interface AssetService{
+Asset PostData(Asset asset);
+List<Asset>getval();
+Asset getData(int id);
+StudentEntity updateData(int id,StudentEntity entity);
 }
