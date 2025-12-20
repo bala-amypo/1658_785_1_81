c45c15736e3ba613f1cb64034fda3da045c52086
bala-amypo <bala@amypo.in>
2025-12-20 08:06:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/DisposalRecord.java
+++ src/main/java/com/example/demo/entity/DisposalRecord.java
@@ -1,5 +1,36 @@
-package com.example.demo.entity;
+package  com.example.demo.entity;
+import java.util.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.PrePersist;
 
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="disposal_records")
 public class DisposalRecord{
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Asset asset;
+    private String disposalMethod;
+    private LocalDate disposalDate;
+    private User approvedBy;  
+    private String notes;
+    private LocalDateTime createA
+
+@PrePersist
+public void onCreate(){
+    if(this.evenDate==null)
+    this.eventDate=LocalDateTime.now();
+}
 }
