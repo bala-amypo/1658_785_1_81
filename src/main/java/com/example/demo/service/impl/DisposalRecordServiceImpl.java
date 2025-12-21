b2ec0e95e7e2ab6e1397949cc1eb80b612758610
bala-amypo <bala@amypo.in>
2025-12-21 18:56:35 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DisposalRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DisposalRecordServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service.Impl;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.DisposalRecord;
 import com.example.demo.repository.DisposalRecordRepository;
--- src/main/java/com/example/demo/service/impl/LifecycleEventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LifecycleEventServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service.Impl;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.LifecycleEvent;
 import com.example.demo.repository.LifecycleEventRepository;
