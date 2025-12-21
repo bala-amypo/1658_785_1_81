fd671061f3b91d2693be5c770e5365874ec28446
bala-amypo <bala@amypo.in>
2025-12-21 18:03:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/TransferRecordController.java
+++ src/main/java/com/example/demo/controller/TransferRecordController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.TransferController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
