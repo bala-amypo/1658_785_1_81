047d9ccd875612076bb373db95aa8296f1d07d51
bala-amypo <bala@amypo.in>
2025-12-21 16:34:06 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -1,40 +1,45 @@
-package com.example.demo.service.Impl;
-import org.springframework.stereotype.Service;
-import com.example.demo.service.UserService;
-import com.example.demo.entity.User;
-import com.example.demo.exception.ResourceNotException;
-import org.springframework,security.crypto.password.PasswordEncoder;
-import com.example.demo.repository.UserRepository;
+package com.example.demo.service.impl;
+
 import java.util.List;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+
 @Service
-public class UserServiceImpl implements UserService{
-private final UserRepository userRepo;
-private final PasswordEncoder passwordEncoder;
-public UserServiceImpl(UserRepository,userRepo,PasswordEncoder passwordEncoder){
-    this.userRepo=userRepo;
-    this.passwordEncoder=passwordEncoder;
-}
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepo,
+                           PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
-public User PostData(User user){
-if(userRepo.existsByEmail(user.getEmail())){
-throw new RuntimeException("Email already in use");
-}
-if(user.getPassword().length()<8){
-    throw new   RuntimeException("Password must be at least 8 characters");
-}if(user.getDepartment()==null){
-    throw new RuntimeException("Department is required");
-}
-user.setPassword(passwordEncoder.encode(user.getPassword()));
-return userRepo.save(user);
+    public User postData(User user) {
+        if (userRepo.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already in use");
+        }
 
-@Override
-public List<User>getAllData(){
-return userRepo.findAll();
-}
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
 
-@Override
-public User getData(int id){
-    return userRepo.findById(id).orElseThrow(()->new RuntimeException("User not found"));
+    @Override
+    public List<User> getAllData() {
+        return userRepo.findAll();
+    }
 
-}
+    @Override
+    public User getData(int id) {
+        return userRepo.findById((long) id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
 }
