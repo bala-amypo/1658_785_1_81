0b99f17f0cd5cc9724a68de51ea2cc6956581254
bala-amypo <bala@amypo.in>
2025-12-21 18:04:35 +0000

    User commit

--- src/main/java/com/example/demo/controller/DisposalRecordController.java
+++ src/main/java/com/example/demo/controller/DisposalRecordController.java
@@ -8,7 +8,6 @@ import com.example.demo.entity.DisposalRecord;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.List;
