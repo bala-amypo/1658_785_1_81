bd763be7f3da74fe187ca84368463b5744019419
bala-amypo <bala@amypo.in>
2025-12-21 16:33:41 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/AssetServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/AssetServiceImpl.java
@@ -1,34 +1,43 @@
-package com.example.demo.service.Impl;
-import org.springframework.stereotype.Service;
-import com.example.demo.service.AssetService;
-import com.example.demo.entity.Asset;
-import com.example.demo.exception.ResourceNotException;
-import com.example.demo.repository.AssetRepository;
+package com.example.demo.service.impl;
+
 import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Asset;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.AssetRepository;
+import com.example.demo.service.AssetService;
+
 @Service
-public class AssetServiceImp implements AssetService{
-private final AssertRepository assetRepo;
-public AssetServiceImpl(AssetRepository assetRepo){
-    this.assetRepo=assetRepo;
-}
+public class AssetServiceImpl implements AssetService {
+
+    private final AssetRepository assetRepo;
+
+    public AssetServiceImpl(AssetRepository assetRepo) {
+        this.assetRepo = assetRepo;
+    }
+
     @Override
-public Asset PostData(Asset asset){
-return assetRepo.save(asset);
-}
-@Override
-public List<Asset>getAllData(){
-return assetRepo.findAll();
-}
+    public Asset postData(Asset asset) {
+        return assetRepo.save(asset);
+    }
 
-@Override
-public Asset getData(int id){
-    return assetRepo.findById((long)id).orElseThrow(()->new ResorceNotFoundException("Asset not found"));
+    @Override
+    public List<Asset> getAllData() {
+        return assetRepo.findAll();
+    }
 
-}
-@Override
-public Asset updateStatus(int id,String status){
-   Asset asset=getData(id);
-   asset.setStatus(status);
-   return assetRepo.save(asset);
-}
+    @Override
+    public Asset getData(int id) {
+        return assetRepo.findById((long) id)
+                .orElseThrow(() -> new ResourceNotFoundException("Asset not found"));
+    }
+
+    @Override
+    public Asset updateStatus(int id, String status) {
+        Asset asset = getData(id);
+        asset.setStatus(status);
+        return assetRepo.save(asset);
+    }
 }
