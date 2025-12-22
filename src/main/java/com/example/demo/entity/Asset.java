4624e132037e09aa47467ccc714de771f57af2d2
bala-amypo <bala@amypo.in>
2025-12-22 02:02:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/Asset.java
+++ src/main/java/com/example/demo/entity/Asset.java
@@ -14,27 +14,32 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @Table(name = "assets")
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+ @NoArgsConstructor
+ @AllArgsConstructor
 public class Asset {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
-    private String assetTag; //
-    private String assetType; // LAPTOP, DESKTOP, etc.
+    private String assetTag; 
+    private String assetType; 
     private String model;
     private LocalDate purchaseDate;
-    private String status; //
+    private String status; 
 
     @ManyToOne
     @JoinColumn(name = "current_holder_id")
-    private User currentHolder; //
+    private User currentHolder; 
     private LocalDateTime createdAt;
 
     @PrePersist
     protected void onCreate() {
-        if (this.status == null) this.status = "AVAILABLE"; //
-        if (this.createdAt == null) this.createdAt = LocalDateTime.now(); //
+        if (this.status == null) 
+        this.status = "AVAILABLE"; 
+        if (this.createdAt == null) 
+        this.createdAt = LocalDateTime.now(); 
     }
 }
