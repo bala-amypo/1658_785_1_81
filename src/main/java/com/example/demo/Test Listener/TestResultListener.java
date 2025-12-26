df1b130098ae4df6649920c0b4404789c409a5f6
bala-amypo <bala@amypo.in>
2025-12-26 06:05:50 +0000

    User commit

--- src/main/java/com/example/demo/Test Listener/TestResultListener.java
+++ src/main/java/com/example/demo/Test Listener/TestResultListener.java
@@ -1,12 +1,4 @@
-// package com.example.demo;
 
-// import org.testng.ITestListener;
-// import org.testng.ITestResult;
-
-// public class TestResultListener implements ITestListener {
-//     public void onTestSuccess(ITestResult result) {}
-//     public void onTestFailure(ITestResult result) {}
-// }
 package com.example.demo;
 
 /**
