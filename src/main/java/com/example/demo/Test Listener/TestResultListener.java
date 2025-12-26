a1478c376d47c82341f15ddbc764b63226a857f9
bala-amypo <bala@amypo.in>
2025-12-26 06:04:06 +0000

    User commit

--- src/main/java/com/example/demo/Test Listener/TestResultListener.java
+++ src/main/java/com/example/demo/Test Listener/TestResultListener.java
@@ -1,9 +1,18 @@
+// package com.example.demo;
+
+// import org.testng.ITestListener;
+// import org.testng.ITestResult;
+
+// public class TestResultListener implements ITestListener {
+//     public void onTestSuccess(ITestResult result) {}
+//     public void onTestFailure(ITestResult result) {}
+// }
 package com.example.demo;
 
-import org.testng.ITestListener;
-import org.testng.ITestResult;
-
-public class TestResultListener implements ITestListener {
-    public void onTestSuccess(ITestResult result) {}
-    public void onTestFailure(ITestResult result) {}
+/**
+ * Dummy class required for compilation.
+ * No TestNG imports allowed in main source.
+ */
+public class TestResultListener {
+    // no methods required
 }
